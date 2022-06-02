30a536ded2fcf3d7a721b090883b8663cd5cd7f4
japolyak <artemkryvolap@gmail.com>
2022-06-02 22:33:16 +0200

    nice

--- g.py
+++ g.py
@@ -1,28 +1,36 @@
-msg_0 = "Enter an equation"
-msg_1 = "Do you even know what numbers are? Stay focused!"
-msg_2 = "Yes ... an interesting math operation. You\'ve slept through all classes, haven\'t you?"
+# msg_0 = "Enter an equation"
+# msg_1 = "Do you even know what numbers are? Stay focused!"
+# msg_2 = "Yes ... an interesting math operation. You\'ve slept through all classes, haven\'t you?"
+#
+# Operators = ('+', '-', '*', '/')
+#
+# while True:
+#     print(msg_0)
+#     calc = input()
+#     calc = calc.split()
+#     try:
+#         x = float(calc[0])
+#         y = float(calc[2])
+#         oper = calc[1]
+#
+#         if oper not in '+-*/':
+#             continue
+#         break
+#
+#     except ValueError:
+#         print(msg_1)
+#         continue
 
-Operators = ('+', '-', '*', '/')
-
-a = True
-
-while a is True:
-    print(msg_0)
-    calc = input()
-    calc = calc.split()
-    try:
-        x = float(calc[0])
-        y = float(calc[2])
-        oper = calc[1]
-
-        for i, r in enumerate(Operators):
-            print(r)
-            if oper == r:
-                break
-            print(i)
-        print(msg_2)
-        a = True
-
-    except ValueError:
-        print(msg_1)
-        a = True
+print(123[0])
+# a = (4, 6, 2)
+# c = 2
+#
+# while True:
+#     for i, n in enumerate(a):
+#         if i == c:
+#             print(type(i))
+#             print('break')
+#             break
+#     break
+#
+# print('koniec')
