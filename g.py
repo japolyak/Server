4d1427e51230c7f0ba1696ed5b2392a12598d7be
japolyak <artemkryvolap@gmail.com>
2022-06-03 01:35:38 +0200

    3 excersise

--- g.py
+++ g.py
@@ -2,13 +2,21 @@ msg_0 = "Enter an equation"
 msg_1 = "Do you even know what numbers are? Stay focused!"
 msg_2 = "Yes ... an interesting math operation. You\'ve slept through all classes, haven\'t you?"
 msg_3 = "Yeah... division by zero. Smart move..."
+msg_4 = "Do you want to store the result? (y / n):"
+msg_5 = "Do you want to continue calculations? (y / n):"
+memory = 0
 
 while True:
     print(msg_0)
     x, oper, y = input().split()
 
     try:
+        if x == "M":
+            x = memory
         x = float(x)
+
+        if y == "M":
+            y = memory
         y = float(y)
 
     except ValueError:
@@ -16,12 +24,43 @@ while True:
         continue
 
     if oper not in '+-*/':
+        print(msg_2)
         continue
 
-    try:
-        result = eval(str(x) + oper + str(y))
+    if oper == '+':
+        result = x + y
         print(result)
+
+    elif oper == '-':
+        result = x - y
+        print(result)
+
+    elif oper == '*':
+        result = x * y
+        print(result)
+
+    elif oper == '/':
+        try:
+            result = x / y
+            print(result)
+
+        except ZeroDivisionError:
+            print(msg_3)
+            continue
+
+    while True:
+        print(msg_4)
+        answer = input()
+        if answer == 'y':
+            memory = result
+            break
+
         break
-    except ZeroDivisionError:
-        print(msg_3)
+
+    print(msg_5)
+    answer_2 = input()
+
+    if answer_2 == 'y':
         continue
+
+    break
