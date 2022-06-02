6bec0dab6ca9ea7e21ee716e61d5a3da7d89b8f8
japolyak <artemkryvolap@gmail.com>
2022-06-03 00:44:33 +0200

    second change

--- g.py
+++ g.py
@@ -1,36 +1,27 @@
-# msg_0 = "Enter an equation"
-# msg_1 = "Do you even know what numbers are? Stay focused!"
-# msg_2 = "Yes ... an interesting math operation. You\'ve slept through all classes, haven\'t you?"
-#
-# Operators = ('+', '-', '*', '/')
-#
-# while True:
-#     print(msg_0)
-#     calc = input()
-#     calc = calc.split()
-#     try:
-#         x = float(calc[0])
-#         y = float(calc[2])
-#         oper = calc[1]
-#
-#         if oper not in '+-*/':
-#             continue
-#         break
-#
-#     except ValueError:
-#         print(msg_1)
-#         continue
+msg_0 = "Enter an equation"
+msg_1 = "Do you even know what numbers are? Stay focused!"
+msg_2 = "Yes ... an interesting math operation. You\'ve slept through all classes, haven\'t you?"
+msg_3 = "Yeah... division by zero. Smart move..."
 
-print(123[0])
-# a = (4, 6, 2)
-# c = 2
-#
-# while True:
-#     for i, n in enumerate(a):
-#         if i == c:
-#             print(type(i))
-#             print('break')
-#             break
-#     break
-#
-# print('koniec')
+while True:
+    print(msg_0)
+    x, oper, y = input().split()
+
+    try:
+        x = float(x)
+        y = float(y)
+
+    except ValueError:
+        print(msg_1)
+        continue
+
+    if oper not in '+-*/':
+        continue
+
+    try:
+        result = eval(str(x) + oper + str(y))
+        print(result)
+        break
+    except ZeroDivisionError:
+        print(msg_3)
+        continue
