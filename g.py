98d38406f1c39add3c74da95681e65f7995fd9ce
japolyak <artemkryvolap@gmail.com>
2022-06-05 13:58:35 +0200

    3.1

--- g.py
+++ g.py
@@ -1,129 +1,125 @@
-msg_ = ("Enter an equation",
-        "Do you even know what numbers are? Stay focused!",
-        "Yes ... an interesting math operation. You\'ve slept through all classes, haven\'t you?",
-        "Yeah... division by zero. Smart move...",
-        "Do you want to store the result? (y / n):",
-        "Do you want to continue calculations? (y / n):",
-        " ... lazy",
-        " ... very lazy",
-        " ... very, very lazy",
-        "You are",
-        "Are you sure? It is only one digit! (y / n)",
-        "Don\'t be silly! It\'s just one number! Add to the memory? (y / n)",
-        "Last chance! Do you really want to embarrass yourself? (y / n)")
-
-memory = 0
+from math import log
 
 
-def is_one_digit(v):
-    if v > -10 and v < 10 and type(v) is int:
-        output = True
-    else:
-        output = False
-    return output
+def time(x):
+    years = x / 12
 
+    while 1 < years < 2:
+        if years % 1 != 0:
+            months = int(x) - 11
 
-def check(v1, v2, v3):
-    msg = ""
-
-    if is_one_digit(v1) and is_one_digit(v2):
-        msg = msg + msg_[6]
-
-    if v1 == 1 or v2 == 1 and v3 == '*':
-        msg = msg + msg_[7]
-
-    if v1 == 0 or v2 == 0 and v3 in '+-*':
-        msg = msg + msg_[8]
-
-    if msg != "":
-        msg = msg_[9] + msg
-        print(msg)
-
-
-def in_put(a):
-    if a == "M":
-        a = memory
-        return a
-    try:
-        a = int(a)
-    except ValueError:
-        a = float(a)
-    return a
-
-
-def ostatok(a):
-    if a % int(a) == 0:
-        return int(a)
-    return a
-
-
-while True:
-    print(msg_[0])
-    x, oper, y = input().split()
-
-    try:
-        x = in_put(x)
-        y = in_put(y)
-
-        x = ostatok(x)
-        y = ostatok(y)
-
-
-    except ValueError:
-        print(msg_[1])
-        continue
-
-    if oper not in '+-*/':
-        print(msg_[2])
-        continue
-
-    check(x, y, oper)
-
-    if oper == '+':
-        result = x + y
-
-    elif oper == '-':
-        result = x - y
-
-    elif oper == '*':
-        result = x * y
-
-    elif oper == '/':
-        try:
-            result = x / y
-
-        except ZeroDivisionError:
-            print(msg_[3])
-            continue
-
-    print(float(result))
-
-    while True:
-        print(msg_[4])
-        answer = input()
-        if answer == 'y':
-            if is_one_digit(result) is True:
-                msg_index = 10
-                while True:
-                    print(msg_[msg_index])
-                    answer_3 = input()
-                    if answer_3 == 'y':
-                        if msg_index < 12:
-                            msg_index += 1
-                            continue
-                        memory = result
-                        break
-                    break
+            if months == 1:
+                print("It will take 1 year and 1 month to repay this loan!")
                 break
-            memory = result
+            print(f"It will take 1 year and {int(months)} months to repay this loan!")
             break
+
+        months = x - 12
+        if months == 1:
+            print("It will take 1 year and 1 month to repay this loan!")
+            break
+        print(f"It will take 1 year and {int(months)} months to repay this loan!")
         break
 
-    print(memory)
-    print(msg_[5])
-    answer_2 = input()
+    while years > 2:
+        if years % 1 != 0:
+            months = int(x) + 1
 
-    if answer_2 == 'y':
-        continue
+            if months == 1:
+                print(f"It will take {int(years)} years and 1 month to repay this loan!")
+                break
+            print(f"It will take {int(years)} years and {months - int(years) * 12} months to repay this loan!")
+            break
 
-    break
+        if months == 1:
+            print(f"It will take {int(years)} years and 1 month to repay this loan!")
+            break
+        print(f"It will take {int(years)} years and {months - int(years) * 12} months to repay this loan!")
+        break
+
+    return answer
+
+
+def n_mothly_fayments(x, y, z):
+    i = z / 1200
+    n = log((y / (y - i * x)), i + 1)
+
+    while True:
+        if n <= 1:
+            print("It will take 1 month to repay this loan!")
+            break
+
+        elif 1 < n <= 11:
+
+            if n % 1 != 0:
+                n = int(n) + 1
+
+            print(f"It will take {n} months to repay this loan!")
+            break
+
+        elif 11 < n < 12:
+
+            if n % 1 != 0:
+                n = int(n) + 1
+
+            print("It will take 1 year to repay this loan!")
+            break
+
+        elif n % 12 == 0:
+            if n / 12 == 1:
+                print("It will take 1 year to repay this loan!")
+                break
+            print(f"It will take {int(n / 12)} years to repay this loan!")
+            break
+
+        else:
+            time(n)
+            break
+
+    return
+
+
+def ann_m_payment_amount(x, y, i):
+    return answer
+
+
+def loan_principal(x, y, i):
+    return answer
+
+
+print(f"""What do you want to calculate?
+type "n" for number of monthly payments,
+type "a" for annuity monthly payment amount,
+type "p" for loan principal:""")
+
+answer = input()
+
+if answer == "n":
+    print("Enter the loan principal:")
+    loan_prin = int(input())
+    print("Enter the monthly payment:")
+    m_payment = int(input())
+    print("Enter the loan interest:")
+    loan_int = int(input())
+
+    n_mothly_fayments(loan_prin, m_payment, loan_int)
+
+elif answer == "a":
+    print("Enter the loan principal:")
+    loan_prin = int(input())
+    print("Enter the number of periods:")
+    n_periods = int(input())
+    print("Enter the loan interest:")
+    loan_int = int(input())
+
+    n_mothly_fayments(loan_prin, n_periods, loan_int)
+
+elif answer == "p":
+    print("Enter the annuity payment:")
+    ann_payment = float(input())
+    print("Enter the number of periods:")
+    n_periods = int(input())
+    print("Enter the loan interest:")
+    loan_int = float(input())
+
+    loan_principal(ann_payment, n_periods, loan_int)
